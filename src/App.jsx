93ac5dfeb8f72a2910e637fede01f1af0a08ab97
ikhdaakmalia <punyaamelaja@gmail.com>
2024-06-22 11:34:29 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import gambar from "./assets/tebak.jpg";
+
+function App() {
+
+  return (
+    <>
+      <h1>Ikhda Akmaia Putri</h1>
+      <img src={gambar} alt="gambar" />
+    </>
+  )
+}
+
+export default App
